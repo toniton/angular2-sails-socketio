81fa1055f8aba22b13a724d8fc7f6ecc564d07f8
Toniton <akinjiolatoni@gmail.com>
2017-04-12 01:00:10 +0100

    exported method enum

--- index.ts
+++ index.ts
@@ -1,11 +1,1 @@
-// import { SocketIOAuthenticate, SocketIOModule, SocketIOModel, SocketIOQuery, SocketIOResponse, SocketIOConfig } from "./src/index";
-
-// export {
-//     SocketIOAuthenticate,
-//     SocketIOModule,
-//     SocketIOModel,
-//     SocketIOQuery,
-//     SocketIOResponse,
-//     SocketIOConfig
-// }
 export * from './src/index';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 export { SocketIOModule } from './socketio.module';
-export { SocketIOModel } from './socketio.model';
+export { SocketIOModel, METHOD } from './socketio.model';
 export { SocketIOQuery } from './socketio.query';
 export { SocketIOResponse } from './socketio.response';
 export { SocketIOAuthenticate } from './socketio.authenticate';
